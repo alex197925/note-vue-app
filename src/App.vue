324c52a4c2877e414bf447661286cf65d860e8ab
tolok oleksandr <tolok.oleksandr@gmail.com>
2023-01-18 12:26:17 +0100

    Added html and css

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,56 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
+<!-- @format -->
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
-    <TheWelcome />
+    <div class="container">
+      <header>
+        <h1>Notes</h1>
+        <button>+</button>
+      </header>
+
+      <div class="card-container">
+        <div class="card">
+          <p class="main-text"></p>
+          <p class="date"></p>
+        </div>
+      </div>
+    </div>
   </main>
 </template>
 
 <style scoped>
+main {
+  height: 100vh;
+  width: 100vw;
+}
+
+.container {
+  max-width: 1000px;
+  margin: 0 auto;
+  padding: 10px;
+}
+
 header {
-  line-height: 1.5;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+h1 {
+  font-weight: bold;
+  margin-bottom: 25px;
+  font-size: 75px;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+button {
+  border: none;
+  padding: 10px;
+  width: 50px;
+  height: 50px;
+  cursor: pointer;
+  background-color: rgb(21, 20, 20);
+  color: #fff;
+  font-size: 20px;
+  border-radius: 100%;
 }
 </style>
